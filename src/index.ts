dc6e9ed39d327afbd3cebac31c1e9969fd231865
Sergei Koshel <skoshel@aligntech.com>
2024-01-19 10:42:59 +0100

    Use simlayer for special symbols

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import {
   FromKeyCode,
   ifInputSource,
-  layer,
   map,
   NumberKeyValue,
   rule,
+  simlayer,
   ToKeyParam,
   withMapper,
   withModifier,
@@ -49,7 +49,7 @@ writeToProfile('SK', [
   // (If you type fast, use simlayer instead, see https://evan-liu.github.io/karabiner.ts/rules/simlayer)
   // to make it easier to write '←' instead of 'left_arrow'.
   // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
-  layer('/', 'symbol-mode').manipulators([
+  simlayer('/', 'symbol-mode').manipulators([
     //     / + [ 1    2    3    4    5 ] =>
     withMapper(['⌘', '⌥', '⌃', '⇧', '⇪'])((k, i) =>
       map((i + 1) as NumberKeyValue).toPaste(k),
